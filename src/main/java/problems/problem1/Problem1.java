14aceaca0160e0a40817c311624ded84a7453b52
Anthony J Clink <anthonyjclink@gmail.com>
2016-11-09 12:27:25 -0800

    Added starting tests and main program for problem 1

---
+++ src/main/java/problems/problem1/Problem1.java
@@ -0,0 +1,17 @@
+package problems.problem1;
+
+/**
+ * 
+ * Takes command line arguments in the form of a comma separated list and prints a the contents reverse order. If a token has an even number of characters, also reverse its text.
+ * 
+ *
+ */
+public class Problem1 {
+	
+	public static void main(String[] args){
+		
+	
+	}
+	
+	
+}
---
+++ src/test/java/problems/problem1/InputReaderUnitTest.java
@@ -0,0 +1,23 @@
+package problems.problem1;
+
+import org.junit.Test;
+
+public class InputReaderUnitTest {
+
+	@Test
+	public void inputReaderProperlyParsesTheInputAsACommaSeparatedList() {
+
+	}
+
+	@Test
+	public void inputReaderProperlyJoinsTheJavaArgsArrayIfTheInputWasMistyped() {
+
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void nullDataIsNotAllowedInAnyField() {
+
+	}
+
+	
+}
